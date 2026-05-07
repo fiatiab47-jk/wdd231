bf654bf87444ee523931e9012998ac84cb421ece
fiatiab47-jk <afiati@byupathway.edu>
2026-05-06 22:38:10 -0700

    Add scrolling effect

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -4,5 +4,13 @@ const navBar = document.querySelector('#nav-bar');
 navButton.addEventListener('click', () => {
     navButton.classList.toggle('show');
     navBar.classList.toggle('show');
+
+    // Scroll to top when opening
+    if (navBar.classList.contains('show')) {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    }
 });
 
